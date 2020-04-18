d852882b1858f3f8c94f851fc536fcf977a19b28
MacarenaPesce <maqarena.agostina.pesce@gmail.com>
2020-04-18 20:38:49 -0300

    Empezando el broker

---
+++ Broker/Broker.h
@@ -0,0 +1,18 @@
+/*
+ * Team.h
+ *
+ *  Created on: 11 abr. 2020
+ *      Author: utnso
+ */
+
+#ifndef TEAM_H_
+#define TEAM_H_
+
+#include "Contexto_team.h"
+#include "Herramientas_team.h"
+
+void recibir_mensaje_de_texto(int, int);
+void iniciar_servidor(void);
+void esperar_cliente(int);
+
+#endif /* TEAM_H_ */
---
+++ Broker/Contexto_broker.c
@@ -0,0 +1,24 @@
+/*
+ * Contexto_team.c
+ *
+ *  Created on: 11 abr. 2020
+ *      Author: utnso
+ */
+
+#include "Contexto_team.h"
+
+/* Global variables*/
+char** posiciones_entrenadores;
+char** pokemon_entrenadores;
+char** objetivos_entrenadores;
+int tiempo_reconexion;
+int retardo_ciclo_cpu;
+char* algoritmo_planificacion;
+int quantum;
+char* ip_broker;
+int estimacion_inicial;
+char* puerto_broker;
+char* log_file;
+
+t_log* team_logger;
+t_config* config;
