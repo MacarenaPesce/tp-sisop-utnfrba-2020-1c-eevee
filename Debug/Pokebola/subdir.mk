1290450abb2b2aa96874e20e3d4551ad44de6762
Lucas Julian Guerci <lucas.guerci@gmail.com>
2020-04-20 19:52:56 -0300

    Modificada logica de logs segun caso

--- Debug/Pokebola/subdir.mk
+++ Debug/Pokebola/subdir.mk
@@ -4,26 +4,20 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Contexto_team.c \
-../Herramientas_team.c \
-../Team.c 
+../Pokebola/Pokebola.c 
 
 OBJS += \
-./Contexto_team.o \
-./Herramientas_team.o \
-./Team.o 
+./Pokebola/Pokebola.o 
 
 C_DEPS += \
-./Contexto_team.d \
-./Herramientas_team.d \
-./Team.d 
+./Pokebola/Pokebola.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-%.o: ../%.c
+Pokebola/%.o: ../Pokebola/%.c
 	@echo 'Building file: $<'
-	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/workspace/tp-2020-1c-eevee/Pokebola" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Invoking: Cross GCC Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
