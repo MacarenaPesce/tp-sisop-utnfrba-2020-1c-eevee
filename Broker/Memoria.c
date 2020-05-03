d28db969307eaed04e76cd2a8403d1751cee5d22
MacarenaPesce <maqarena.agostina.pesce@gmail.com>
2020-05-03 20:22:37 -0300

    Edite HerramientasBroker, cargue bien la config de Broker, empece la logica de administracion y de los algoritmos

--- Broker/Memoria.c
+++ Broker/Memoria.c
@@ -1,12 +1,14 @@
 #include "Memoria.h"
 
+//--------------- ADMINISTRACION DE MEMORIA-------------------
+
 void* AsignarMemoriaInicial(int tamanio_en_bytes, t_list* lista_memoria){
     
     /* Asigno la memoria a un puntero auxiliar y la inicializo en cero */
     void* memoria_inicial = malloc(tamanio_en_bytes*sizeof(char));    
     memset(memoria_inicial, 0, tamanio_en_bytes*sizeof(char));
    
-    /* Genero el bloque de memoria */
+    /* Genero el bloque de memoria inicial*/
     t_bloque_memoria *bloque;
     bloque->tamanio = tamanio_en_bytes;
     bloque->esta_vacio = true;
@@ -29,8 +31,10 @@ void LiberarMemoriaInicial(void* bloque_memoria_inicial,t_list* lista_memoria){
     return;
 }
 
-void* AsignarBloqueMemoria(t_list* lista_memoria, int tamanio_en_bytes){
+void* AsignarBloqueMemoria(t_list* lista_memoria, int tamanio_en_bytes, char* algoritmo_planificacion){
     
+    if( )
+
 }
 
 void LiberarBloqueMemoria(t_bloque_memoria *bloque){
@@ -87,3 +91,99 @@ int CalcularTamanioBloqueMemoria(char* inicio_del_bloque, int tamanio_en_bytes){
 
 }
 
+//-----------------------------ELECCION DE PARTICION LIBRE--------------------
+
+//------------------------------FIRST FIT------------------------------------
+
+/*
+    Recorrer la memoria o la lista?
+    Buscar la primer particion libre --- buscarParticion()
+    fijarme si entra --- la ocupo o sigo buscando
+    si no entra, hago una compactacion
+    vuelvo a fijarme si entra, si no entra borro algo ---- que se borra?
+
+
+
+*/
+
+//----------------------------FIN FIRST FIT----------------------------------
+
+
+
+
+//------------------------------BEST FIT-------------------------------------
+
+
+//----------------------------FIN DE BEST FIT---------------------------------
+
+//------------------------------FIN DE ELECCION DE PARTICION------------------
+
+
+
+
+
+//----------------------------ELIMINAR UNA PARTICION-----------------------
+
+//------------------------------FIFO--------------------------------
+
+
+//------------------------------FIN DE FIFO--------------------------------
+
+
+//------------------------------LRU--------------------------------
+
+//--------------------------FIN DE LRU--------------------------------
+
+//------------------------FIN ELIMINAR UNA PARTICION-----------------------
+
+
+uint32_t muse_alloc_mem(int programId, uint32_t tam) {
+    // ajusta la memoria solicitada como múltiplo entero del tamaño de la página
+    int cantPaginas = tam / MEMORIA.tamPagina + !!(tam % MEMORIA.tamPagina);
+    tam = cantPaginas * MEMORIA.tamPagina;
+    Segmento *s = MEMORIA.tabla;
+    Bloque *b = NULL;
+    // intenta asignar en el primer segmento
+    if(!s->progId || s->progId == programId) {
+        b = alojarBloque(s, tam);
+    }
+    // mientras no logre asignar y haya segmentos más adelante
+    while(s->next && !b) {
+        while(s->next->progId && s->next->progId != programId) {
+            s = s->next;
+        }
+        if(s->next) {
+            s = s->next;
+            b = alojarBloque(s, tam);
+        }
+    }
+    // s apunta ahora al segmento donde fue asignado el bloque
+    // o al último segmento de la lista, si no fue asignado
+    if(b) { // si el bloque fue asignado
+            // establece el programa como propietario del segmento
+        s->progId = programId;
+    } else {    // si no pudo ser asignado
+        // calcula hasta dónde llegará un nuevo segmento a continuación del último
+    	uint32_t mem = s->mem + s->tam + METAD + tam;
+        // si hay espacio crea el nuevo segmento y le asigna el bloque
+        if(mem < MEMORIA.tamMemoria) {
+            s->next = nuevoSegmento(programId, s->mem + s->tam, s->next);
+            s = s->next;
+            b = alojarBloque(s, tam);
+            s->progId = programId;
+        }
+    }
+    s->memLiberada = 0;
+    free(s);
+    // si no logra asignar el bloque es porque se agotó la memoria disponible
+    if(b){
+    	funMem = iniciar_logger_fun();
+    	log_info(funMem, "Alloc realizado con exito. Proceso: %d", programId);
+    	free(funMem);
+    	return b->data;
+    }else{
+    	return 0; //sacar al resolver el tema de clock
+    	//activar clock moficado wachines
+    }
+    //return b ? b->data : 0; //si retorna 0 es segmentation fault
+}
--- Broker/Memoria.h
+++ Broker/Memoria.h
@@ -7,11 +7,26 @@
 #include <stdbool.h>
 #include <commons/collections/list.h>
 
+//--------------- ADMINISTRACION DE MEMORIA-------------------
+
+t_list* lista_particiones; //lista de particiones de la memoria
+
+//Estructura para reservar la memoria inicial
 typedef struct{
     int tamanio;
     bool esta_vacio;
     float* payload;
 }t_bloque_memoria;
+
+//Estructura para particiones de la memoria inicial
+/*
+typedef struct{
+    int id_particion;
+    int tamanio;
+    int esta_vacio;
+    //int comienzo;
+    //int fin;
+}t_particion;*/    
 
 void* AsignarMemoriaInicial(int tamanio_en_bytes, t_list* lista_memoria);
 void  LiberarMemoriaInicial(void* bloque_memoria_inicial,t_list* lista_memoria);
