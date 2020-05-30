f9262077733d168f528e7151baddc4ee1c140a71
MacarenaPesce <maqarena.agostina.pesce@gmail.com>
2020-05-29 23:58:32 -0300

    calculando bytes de los pedidos

---
+++ Broker/utils.c
@@ -0,0 +1,51 @@
+
+
+
+int calcularBytes (t_paquete* paquete){ 
+
+    int bytes = 0;
+
+    switch(paquete-> operacion){
+			case NEW_POKEMON:
+				puts(" Recibi un NEW POKEMON");
+				
+                //bytes = ;
+
+				return bytes;
+			case :
+				puts(" Recibi un ");
+
+                //bytes = ;
+
+				return bytes;
+			case :
+				puts(" Recibi un ");
+
+                //bytes = ;
+
+				return bytes;
+			case :
+				puts(" Recibi un ");
+
+                //bytes = ;
+
+				return bytes;
+			case :
+				puts(" Recibi un ");
+
+                //bytes = ;
+
+				return bytes;
+			case :
+				puts(" Recibi un ");
+
+                //bytes = ;
+
+				return bytes;
+			default:
+				break;
+		}
+
+
+    return bytes;
+}
