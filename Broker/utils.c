25c01c8341d8d3064d4cdfcbcfd342eb028745a9
MacarenaPesce <maqarena.agostina.pesce@gmail.com>
2020-05-30 09:39:10 -0300

    calcular bytes de appeared y get

--- Broker/utils.c
+++ Broker/utils.c
@@ -9,7 +9,7 @@ int calcularBytes (t_packed* paquete){
 	//ME FALTA AGREGAR UN ENUM O ALGO PARA PODER SABER QUE OPERACION HACE, PAQUETE->OPERACION NO ES!!!
 
 	//NO ESTOY SEGURA SI ELIMINAR PAQUETE ESTA BIEN, Y CUANDO HACER EL FREE PKMN PORQUE LE ESTOY RESERVANDO MEMORIA
-	
+
 
     switch(paquete-> operacion){
 			case NEW_POKEMON:
@@ -24,7 +24,7 @@ int calcularBytes (t_packed* paquete){
 					/*Libero la memoria del paquete*/
 					eliminar_mensaje(paquete);
 
-				//4 bytes por cada uint_32, y tengo 4 uint_32 -> 4*4 
+				//4 bytes por cada uint_32, y tengo 4 uint_32 -> 4 * 4 = 16 
 				//mas el largo del nombre del pokemon
 
                 bytes = 16 + (strlen(pkmn->pokemon));
@@ -34,7 +34,7 @@ int calcularBytes (t_packed* paquete){
 				puts(" Recibi un LOCALIZED POKEMON");
 
 					t_localized_pokemon* pkmn;
-					pkmn =(t_new_pokemon*)malloc(sizeof(t_new_pokemon));
+					pkmn =(t_localized_pokemon*)malloc(sizeof(t_localized_pokemon));
 
 					/* Apunto a los datos del mensaje */
 					pkmn = paquete->mensaje;
@@ -45,19 +45,43 @@ int calcularBytes (t_packed* paquete){
 				//tengo 8 uint_32 -> 4 * 8 = 32
 				//mas el largo del nombre por pokemon
 
-                bytes = 32 + (strlen(pkmn->pokemon);
+                bytes = 32 + (strlen(pkmn->pokemon));
 
 				return bytes;
 			case GET_POKEMON:
 				puts(" Recibi un GET POKEMON");
 
-                //bytes = ;
+					t_get_pokemon* pkmn;
+					pkmn =(t_get_pokemon*)malloc(sizeof(t_get_pokemon));
+
+					/* Apunto a los datos del mensaje */
+					pkmn = paquete->mensaje;
+
+					/*Libero la memoria del paquete*/
+					eliminar_mensaje(paquete);
+                
+				//tengo 1 sólo uint_32 -> 4 bytes
+				//mas el largo del nombre del pokemon
+
+				bytes = 4 + (strlen(pkmn->pokemon));
 
 				return bytes;
 			case APPEARED_POKEMON:
 				puts(" Recibi un APPEARED POKEMON");
 
-                //bytes = ;
+                	t_appeared_pokemon* pkmn;
+					pkmn =(t_appeared_pokemon*)malloc(sizeof(t_appeared_pokemon));
+
+					/* Apunto a los datos del mensaje */
+					pkmn = paquete->mensaje;
+
+					/*Libero la memoria del paquete*/
+					eliminar_mensaje(paquete);
+                
+				//tengo 3 uint_32 -> 4 * 3 = 12
+				//mas el largo del nombre del pokemon
+
+				bytes = 12 + (strlen(pkmn->pokemon));
 
 				return bytes;
 			case CATCH_POKEMON:
