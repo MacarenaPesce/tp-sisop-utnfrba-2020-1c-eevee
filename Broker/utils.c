3434e9aee797fe6ad9280811d3f93e1298e4811d
MacarenaPesce <maqarena.agostina.pesce@gmail.com>
2020-05-30 00:27:32 -0300

    calculando bytes de los pedidos

--- Broker/utils.c
+++ Broker/utils.c
@@ -5,10 +5,14 @@ int calcularBytes (t_paquete* paquete){
 
     int bytes = 0;
 
+    _recuperar_mensaje(paquete-> mensaje , paquete);
+    
     switch(paquete-> operacion){
 			case NEW_POKEMON:
 				puts(" Recibi un NEW POKEMON");
 				
+                
+
                 //bytes = ;
 
 				return bytes;
