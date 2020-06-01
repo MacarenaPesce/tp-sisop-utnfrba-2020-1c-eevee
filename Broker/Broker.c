bbe9f0228fe7b2f52e84133b269824b8a639b34d
Lucas Julian Guerci <lucas.guerci@gmail.com>
2020-06-01 00:03:20 -0300

    Agregada makefile pokebola

--- Broker/Broker.c
+++ Broker/Broker.c
@@ -28,61 +28,74 @@ void iniciar_servidor(void){
 
     freeaddrinfo(servinfo);
 
-    while(1)
-    	esperar_cliente(socket_servidor);
+	pthread_t hilo_sockets;
+	
+	pthread_create(&hilo_sockets,NULL,esperar_cliente,socket_servidor);
 }
 
-void esperar_cliente(int socket_servidor){
-	struct sockaddr_in dir_cliente;
-	int tam_direccion = sizeof(struct sockaddr_in);
-	int socket_cliente = accept(socket_servidor, (void*) &dir_cliente, &tam_direccion);
-	logger(escribir_loguear,l_trace,"\nSe aceptó un nuevo cliente");
+void* esperar_cliente(void* socket_servidor){
+	while(1){
+		int* int_socket_servidor = (int*) socket_servidor;
 
+		struct sockaddr_in dir_cliente;
 
+		int tam_direccion = sizeof(struct sockaddr_in);
 
-	//Por ahora queda en un loop esperando que le lleguen cosas, no es multihilo, eso esta claro.
-	//Todavia queda definir si team y gamecard seran servers multihilo o no. Necesitamos definirlo urgente, porque meter el select
-	//despues es una fiaca. Tambien tenemos que ver si el mensaje lo ingresamos por consola o no. Ahora a team le llega algo hardcodeado.
+		int socket_cliente = accept(socket_servidor, (void*) &dir_cliente, &tam_direccion);
+
+		//Iniciar un hilo por cliente
+		if(socket_cliente != -1){
+
+			pthread_t hilo_cliente;
+
+			void *cliente = (void*)malloc(sizeof(int));
+
+			cliente = socket_cliente;
+
+			pthread_create(&hilo_cliente,NULL,ObtenerMensaje,cliente);
+
+			logger(escribir_loguear,l_trace,"\nSe aceptó un nuevo cliente");
+
+		}
 	
-	//Creo un paquete y recibo el mensaje
-	t_packed* paquete;
-	paquete = recibir_mensaje(socket_cliente);
+	}
+	//return NULL;
 
-	//Esto me devuelve el paquete con todos los datos
-	/* El nro de operacion y cola de mensajes indican el 
-	   tipo de estructura que contiene el paquete        */
-	printf("operacion: %d \n",paquete->operacion);
-	printf("cola_de_mensajes: %d \n",paquete->cola_de_mensajes);
-	printf("id_correlacional: %d  \n",paquete->id_correlacional);
-	printf("id_mensaje: %d \n",paquete->id_mensaje);
-	printf("tamanio_payload: %d \n",paquete->tamanio_payload);
+}
 
-	/* Genero un puntero de ese tipo y lo inicializo */
-	t_new_pokemon* pkmn;
-	pkmn =(t_new_pokemon*)malloc(sizeof(t_new_pokemon));
+void* ObtenerMensaje(void* cliente){
 
-	/* Apunto a los datos del mensaje */
-	pkmn = paquete->mensaje;
+		int socket_cliente = (int*)cliente;
 
-	/*Libero la memoria del paquete*/
-	eliminar_mensaje(paquete);
+		//Creo un paquete y recibo el mensaje
+		t_packed* paquete;
+		paquete = recibir_mensaje(socket_cliente);
 
-	/* Ya puedo usar mi copia de la estructura enviada*/
-	printf("posx: %d \n",pkmn->coordenadas.posx);
-	printf("posy: %d \n",pkmn->coordenadas.posy);
-	printf("cantidad: %d \n",pkmn->cantidad);
-	printf("pokemon: %s \n",pkmn->pokemon);
+		//Esto me devuelve el paquete con todos los datos
+		/* El nro de operacion y cola de mensajes indican el 
+		tipo de estructura que contiene el paquete        */
+		printf("operacion: %d \n",paquete->operacion);
+		printf("cola_de_mensajes: %d \n",paquete->cola_de_mensajes);
+		printf("id_correlacional: %d  \n",paquete->id_correlacional);
+		printf("id_mensaje: %d \n",paquete->id_mensaje);
+		printf("tamanio_payload: %d \n",paquete->tamanio_payload);
 
+		/* Genero un puntero de ese tipo y lo inicializo */
+		t_new_pokemon* pkmn;
+		pkmn =(t_new_pokemon*)malloc(sizeof(t_new_pokemon));
 
+		/* Apunto a los datos del mensaje */
+		pkmn = paquete->mensaje;
 
+		/*Libero la memoria del paquete*/
+		eliminar_mensaje(paquete);
 
-
-
-
-
-
-
-//	logger(escribir_loguear,paquete->tamanio_payload);
+		/* Ya puedo usar mi copia de la estructura enviada*/
+		printf("posx: %d \n",pkmn->coordenadas.posx);
+		printf("posy: %d \n",pkmn->coordenadas.posy);
+		printf("cantidad: %d \n",pkmn->cantidad);
+		printf("pokemon: %s \n",pkmn->pokemon);
+	
 }
 
 
@@ -98,5 +111,7 @@ int main(){
 	int tamanio_memoria_inicial = 8000;
 
 	//primer_bloque = AsignarMemoriaInicial(tamanio_memoria_inicial,lista_memoria);
-
+	while(true){
+	
+	}
 }
