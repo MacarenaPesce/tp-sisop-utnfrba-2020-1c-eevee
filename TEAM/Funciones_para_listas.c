9a96c6fec925a87186c6875f240fc133e2734b0d
aramontoya07 <aramontoya07@gmail.com>
2020-06-13 03:30:43 -0300

    funciones para listas de chars ahora funcionan con listas de pokes vacias

--- TEAM/Funciones_para_listas.c
+++ TEAM/Funciones_para_listas.c
@@ -41,12 +41,16 @@ void separar_listas_pokemones(char *string, t_list* lista){
 }
 
 void separar_pokemones_de_entrenador(char* pokemones_de_entrenador, t_list* lista){
-	char** pokes = string_split(pokemones_de_entrenador, "|");
-	string_iterate_lines_with_list(pokes, lista, agregar_a_lista_pokemones);
-	free(pokes);
+	char* un_char = "|\0";
+	if(pokemones_de_entrenador == NULL){
+	} else if(!(string_contains(pokemones_de_entrenador, un_char))) {
+		agregar_a_lista_pokemones(pokemones_de_entrenador, lista);
+	} else {
+		char**pokes = string_split(pokemones_de_entrenador, "|");
+		string_iterate_lines_with_list(pokes, lista, agregar_a_lista_pokemones);
+		free(pokes);
+	}
 }
-
-
 
 void agregar_a_lista_pokemones(char* especie, t_list* lista){
 	if (especie != NULL) {
@@ -62,41 +66,46 @@ void agregar_objetivo(char* especie, uint32_t cantidad, t_list* lista){
 		objetivo->cantidad_atrapada = 0;
 		list_add(lista, (void*)objetivo);
 	} else {
+		if(especie != NULL){
 		t_objetivo_entrenador* objetivo = malloc(sizeof(t_objetivo_entrenador));
 		objetivo->especie = especie;
 		objetivo->cantidad = cantidad;
 		list_add(lista, (void*)objetivo);
+		} else {
+			printf("El objetivo de este entrenador es nulo");
+		}
 	}
 }
 
 void cargar_objetivos(t_list* pokemones, t_list* lista){
 		char* unPokemon;
 		unPokemon = list_get(pokemones, 0);
-		uint32_t contador = 0;
-		char* un_char = "Ultimo poke\0";
-		char* ultimo_poke = string_from_format("%s\0", un_char);
-		list_add(pokemones, ultimo_poke);
+		if(unPokemon != NULL){
+			uint32_t contador = 0;
+			char* un_char = "Ultimo poke\0";
+			char* ultimo_poke = string_from_format("%s\0", un_char);
+			list_add(pokemones, ultimo_poke);
+			/*Empiezo a cargar a lista de objetivos, con tipo y cantidad de cada uno*/
+			char* especiePokemon;
+			char* otroPokemon;
+			int i = 0;
+			while(pokemones != NULL){
+				especiePokemon = unPokemon;
+				otroPokemon = list_get(pokemones, i);
+				if(otroPokemon == NULL){
+					agregar_objetivo(especiePokemon, contador, lista);
+					break;
+				}
+					if(string_equals_ignore_case(unPokemon,otroPokemon)){
+					contador++;
+					i++;
+					}else{
+						agregar_objetivo(especiePokemon, contador, lista);
+						unPokemon = otroPokemon;
+						contador = 1;
+						i++;
 
-		/*Empiezo a cargar a lista de objetivos, con tipo y cantidad de cada uno*/
-		char* especiePokemon;
-		char* otroPokemon;
-		int i = 0;
-		while(pokemones != NULL){
-			especiePokemon = unPokemon;
-			otroPokemon = list_get(pokemones, i);
-			if(otroPokemon == NULL){
-				agregar_objetivo(especiePokemon, contador, lista);
-				break;
-			}
-			if(string_equals_ignore_case(unPokemon,otroPokemon)){
-				contador++;
-				i++;
-			}else{
-				agregar_objetivo(especiePokemon, contador, lista);
-				unPokemon = otroPokemon;
-				contador = 1;
-				i++;
-
+					}
 			}
 		}
 
