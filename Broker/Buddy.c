d46e5189b9db81f0fe6aff0248bc1bde8ab8bc5c
MacarenaPesce <maqarena.agostina.pesce@gmail.com>
2020-07-11 21:40:56 -0300

    Funcionamiento de buddy y pasos comentado

--- Broker/Buddy.c
+++ Broker/Buddy.c
@@ -13,25 +13,54 @@ extern t_config* config;
 
 extern t_cache_colas* cache_mensajes;
 
+	/*
+		FUNCIONAMIENTO
+
+		Busco particion libre:
+			- Encuentro particion libre -> usa el Best fit, el mejor ajuste,
+						necesito la particion mas pequeña donde quepa
+			- Miro el tamaño de la particion 
+				- si es igual lo alojo
+				- si es menor no me sirve
+				- si es mayor la particiono y vuelvo a revisar el tamaño
+			- Si no encuentro una particion libre donde quepa o si no hay libres:
+				- Corro el algoritmo de eliminacion
+				- Consolidar buddies
+				- Vuelvo a intentar alojas, repito hasta poder alojar
+
+	*/
+
+
 /*
 	Bueno algunas cosas a tener en cuenta para Buddy:
 		- Puedo plantear un arbol paralelo apenas arranque el funcionamiento del broker
+			- Cuando leo el config si es buddy hago esto
 			- En caso de plantear un arbol, me serviria guardar el id padre para consolidar
 			dos buddies.
-			- 
-
+			- Puedo usar XOR para encontrar buddies, pero necesito posiciones relativas 
+			y no reales de memoria
 			- Usamos best fit -> problemas / soluciones de usar FF?
-			- Sin compactacion
-
-
-		
+			- Sin compactacion		
 */
+
+
+
 void buddy_funcionamiento(t_mensaje_cola* estructura_mensaje){
+
+	/*Me fijo si cumple con el tamaño minimo de particion*/
+	int bytes_a_alojar = tamanio_a_alojar(estructura_mensaje->tamanio_mensaje);
+
+	/*Busco el numero potencia de 2 mas cercano*/
+	int bytes_potencia_dos = tamanio_potencia_dos(bytes_a_alojar);
+
+	/*Alojo la partición según el funcionamiento de buddies*/
+	asignar_bloque_BS(estructura_mensaje, bytes_potencia_dos);
+
 
     return;
 }
 
-void asignar_bloque_BS(t_mensaje_cola* estructura_mensaje){
+void asignar_bloque_BS(t_mensaje_cola* estructura_mensaje, int tamanio_particion){
 
 	return;
 }
