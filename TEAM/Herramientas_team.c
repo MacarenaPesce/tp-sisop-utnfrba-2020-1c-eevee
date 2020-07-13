695b8efb46fbc57228894eebf4b62abedc0ddfa7
Lucas Julian Guerci <lucas.guerci@gmail.com>
2020-07-12 23:12:41 -0300

    Agregado makefile a Team

--- TEAM/Herramientas_team.c
+++ TEAM/Herramientas_team.c
@@ -63,7 +63,7 @@ void inicializar_logger(){
 	team_logger = log_create("team.log", "Team", 1, LOG_LEVEL_DEBUG);
 	log_info(team_logger,"Hi, bienvenido a Team");
 
-	team_logger_oficial = log_create("/home/utnso/log_team1", "Team", 0, LOG_LEVEL_DEBUG);
+	team_logger_oficial = log_create("./log_team", "Team", 0, LOG_LEVEL_DEBUG);
 }
 
 void inicializar_semaforos(){
@@ -157,7 +157,7 @@ void inicializar_semaforos_deadlock(){
 }
 
 void inicializar_archivo_de_configuracion(){
-	config = config_create("team.config");
+	config = config_create("../team.config");
 	if(config == NULL){
 		log_info(team_logger,"El archivo de configuracion no existe. Fijate en la carpeta Debug.");
 		terminar_team_correctamente();
