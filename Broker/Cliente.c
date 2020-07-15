6abdab5ea4c5dd6fefb06a414b9136dfd36f6a5c
MacarenaPesce <maqarena.agostina.pesce@gmail.com>
2020-07-14 21:47:17 -0300

    empece pruebas

--- Broker/Cliente.c
+++ Broker/Cliente.c
@@ -2,15 +2,6 @@
 
 t_servidor servidor;
 
-/*  
-	-Primera prueba con pokemons_maca
-	1)
-	Orden: 		new, 	get, 	apparead, 	caugth
-	Resultado:	23		11		19			4
-	Esperado:	23		11		19			4
-
-
-	*/
 
 int main(){
 
@@ -74,11 +65,11 @@ int main(){
 	pthread_t hilo_espera_mensajes;
 	pthread_create(&hilo_espera_mensajes,NULL,esperar_mensajes,(void*)&socket_get_pokemon);
  */
-
+/*
 	ack = enviar_get_pokemon(&servidor,-1, &get_pokemon);
 	free(ack);
 
-/* 
+
 	ack = enviar_appeared_pokemon(&servidor,-1, &appeared_pokemon);
 	free(ack);
 
@@ -98,52 +89,6 @@ int main(){
 	free(ack);
 */
 
-	/*Pokemons de prueba para Macaa*/
-	t_new_pokemon new_pokemon_maca;
-	new_pokemon_maca.coordenadas.posx = 5;
-	new_pokemon_maca.coordenadas.posy = 10;
-	new_pokemon_maca.cantidad = 2;
-	new_pokemon_maca.pokemon = "Pikachu";
-
-	t_get_pokemon get_pokemon_maca;
-	get_pokemon_maca.pokemon = "Pikachu";
-
-	t_catch_pokemon appeared_pokemon_maca;
-	appeared_pokemon_maca.coordenadas.posx = 1;
-	appeared_pokemon_maca.coordenadas.posy = 5;
-	appeared_pokemon_maca.pokemon = "Pikachu";
-
-	t_caught_pokemon caught_pokemon_maca;
-	caught_pokemon_maca.status = 0;
-
-	t_catch_pokemon appeared_pokemon_maca2;
-	appeared_pokemon_maca2.coordenadas.posx = 1;
-	appeared_pokemon_maca2.coordenadas.posy = 5;
-	appeared_pokemon_maca2.pokemon = "Tengotreintabytes";
-
-	t_new_pokemon new_pokemon_maca2;
-	new_pokemon_maca2.coordenadas.posx = 5;
-	new_pokemon_maca2.coordenadas.posy = 10;
-	new_pokemon_maca2.cantidad = 2;
-	new_pokemon_maca2.pokemon = "TengounnombremuylargoparaochentabytesysigosinllegarAAAAAAAAAAAA";
-
-/* 	ack = enviar_new_pokemon(&servidor,-1,&new_pokemon_maca);
-	free(ack);
-
- 	ack = enviar_get_pokemon(&servidor,-1, &get_pokemon_maca);
-	free(ack); 
-
-	ack = enviar_appeared_pokemon(&servidor,-1, &appeared_pokemon_maca);
-	free(ack);
-
-	ack = enviar_caught_pokemon(&servidor,-1, &caught_pokemon_maca);
-	free(ack);
-
-	ack = enviar_appeared_pokemon(&servidor,-1, &appeared_pokemon_maca2);
-	free(ack);
-
-	ack = enviar_new_pokemon(&servidor,-1, &new_pokemon_maca2);
-	free(ack); */
 
 	printf("TERMINE DE PROBAR LISTO   \n");
 
