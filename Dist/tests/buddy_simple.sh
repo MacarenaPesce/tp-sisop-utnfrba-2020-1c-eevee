e7275ac54f0c839bc0eeefb1c17b4b30aa9f1f09
Lucas Julian Guerci <lucas.guerci@gmail.com>
2020-07-18 04:11:45 -0300

    Corregido string de cola en suscripcion

--- Dist/tests/buddy_simple.sh
+++ Dist/tests/buddy_simple.sh
@@ -6,6 +6,6 @@
 
 ../bin/gameboy BROKER CATCH_POKEMON Onyx 4 5
 
-../bin/gameboy SUSCRIPTOR COLA_NEW_POKEMON 10
+../bin/gameboy SUSCRIPTOR NEW_POKEMON 2
 
 ../bin/gameboy BROKER CATCH_POKEMON Charmander 4 5
